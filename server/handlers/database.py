2616fff11633d73208b103bf56026ed9b1dec2be
Omer Katz <omerk2511@gmail.com>
2020-04-28 15:18:26 +0300

    Fixed a bug in the database implementation (a connection created in one thread cannot be used in another).

--- server/handlers/database.py
+++ server/handlers/database.py
@@ -5,21 +5,17 @@ DATABASE_PATH = 'server\\data\\database.db'
 
 class Database(object):
     def __init__(self):
-        self.connection = lite.connect(DATABASE_PATH)
         self.lock = Lock()
 
-    def close(self):
-        self.connection.close()
-
     def execute(self, *args):
-        with self.lock:
-            cursor = self.connection.cursor()
+        with self.lock, lite.connect(DATABASE_PATH) as connection:
+            cursor = connection.cursor()
             cursor.execute(*args)
-            self.connection.commit()
+            connection.commit()
 
     def fetch(self, *args):
-        with self.lock:
-            cursor = self.connection.cursor()
+        with self.lock, lite.connect(DATABASE_PATH) as connection:
+            cursor = connection.cursor()
             cursor.execute(*args)
             return cursor.fetchall()
 
