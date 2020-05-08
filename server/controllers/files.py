747639ff47d66e61aae9a5aa1391e40ee5738dcc
Omer Katz <omerk2511@gmail.com>
2020-05-08 18:31:15 +0300

    Created a get_file controller.

--- server/controllers/files.py
+++ server/controllers/files.py
@@ -1,7 +1,7 @@
 from common import Codes, Message
 from controller import controller
-from validators import validator, existing_directory, not_existing_file
-from auth import authenticated, in_directory_context
+from validators import validator, existing_directory, existing_file, not_existing_file
+from auth import authenticated, in_directory_context, in_file_context
 from ..models import Groups, UsersGroups, Directories, Files
 
 CREATE_FILE_PAYLOAD = [
@@ -25,5 +25,30 @@ def create_file(payload, user):
         {
             'message': 'Create the file successfully.',
             'id': file_id
+        }
+    )
+
+GET_FILE_PAYLOAD = [
+    ('file', [int])
+]
+
+@controller(Codes.GET_FILE)
+@authenticated
+@validator(GET_FILE_PAYLOAD)
+@existing_file
+@in_file_context
+def get_file(payload, user):
+    f = Files.get(payload['file'])[0]
+    content = Files.read(payload['file'])
+
+    return Message(
+        Codes.SUCCESS,
+        {
+            'message': 'Retrieved the file successfully.',
+            'id': f[0],
+            'name': f[2],
+            'owner': f[3],
+            'directory': f[4],
+            'content': content
         }
     )
