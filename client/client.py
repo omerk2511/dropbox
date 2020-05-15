1817603fa523310a78a59e00470ef11c9ffe75b1
Omer Katz <omerk2511@gmail.com>
2020-05-15 10:49:41 +0300

    Now closing the socket on exit.

--- client/client.py
+++ client/client.py
@@ -1,20 +1,25 @@
+import sys
+
 from handlers import GUI, Data, Connection
 
 HOST = 'localhost'
 PORT = 8000
 
-def main():
+def close():
+    connection.socket.close()
+    gui.destroy()
+
+if __name__ == '__main__':
     gui = GUI()
+    gui.protocol('WM_DELETE_WINDOW', close)
+
     data = Data()
     
     try:
         connection = Connection(HOST, PORT)
     except:
         gui.display_error('Could not connect to the server.')
-        return
+        sys.exit(1)
 
     gui.initialize_frames()
     gui.mainloop()
-
-if __name__ == '__main__':
-    main()
